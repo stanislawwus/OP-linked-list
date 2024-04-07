644d5cd7ca88870cc3086c4cf3417cf2f908afd4
stanislawwus <vus.pn@ucu.edu.ua>
2024-04-07 18:17:23 +0300

    create a full move node folder

---
+++ move node/move_node.py
@@ -0,0 +1,13 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    source.next, dest = dest, source.next
+    return Context(dest, source)
