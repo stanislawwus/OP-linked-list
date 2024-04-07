b519a1cbaac887257290c407fb510fe7e60384ed
stanislawwus <vus.pn@ucu.edu.ua>
2024-04-07 18:51:45 +0300

    create a full recursive reverse folder

---
+++ recursive reverse/recursive_reverse.py
@@ -0,0 +1,13 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+def reverse(head):
+    if not head or not head.next:
+        return head
+    else:
+        _ = reverse(head.next)
+        head.next.next = head
+        head.next = None
+        return _
